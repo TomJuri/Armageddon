d6273f3f1e32e96961f97515094c811f2d880df4
TomJuri <mail@tomjuri.de>
2023-07-29 22:45:21 +0200

    chungus

--- src/main/kotlin/de/tomjuri/armageddon/feature/MovementRecorder.kt
+++ src/main/kotlin/de/tomjuri/armageddon/feature/MovementRecorder.kt
@@ -5,37 +5,12 @@ import de.tomjuri.armageddon.util.mc
 import de.tomjuri.armageddon.util.player
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent
-import java.io.File
-import java.io.FileWriter
-import java.io.PrintWriter
 
 class MovementRecorder {
 
-
-
     private val movements = mutableListOf<Movement>()
-    private var recording = false
-    private var recordingIndex = 0
     private var playing = false
     private var playingIndex = 0
-
-    @SubscribeEvent
-    fun onTickRecord(event: ClientTickEvent) {
-        println(recordingIndex)
-        if(!recording) return
-        movements.add(recordingIndex, Movement(
-            forward = mc.gameSettings.keyBindForward.isKeyDown,
-            left = mc.gameSettings.keyBindLeft.isKeyDown,
-            backwards = mc.gameSettings.keyBindBack.isKeyDown,
-            right = mc.gameSettings.keyBindRight.isKeyDown,
-            sneak = mc.gameSettings.keyBindSneak.isKeyDown,
-            jump = mc.gameSettings.keyBindJump.isKeyDown,
-            attack = mc.gameSettings.keyBindAttack.isKeyDown,
-            yaw = player.rotationYaw,
-            pitch = player.rotationPitch
-        ))
-        recordingIndex++
-    }
 
     @SubscribeEvent
     fun onTickPlayer(event: ClientTickEvent) {
@@ -54,35 +29,13 @@ class MovementRecorder {
         if(playingIndex == movements.size) playing = false
     }
 
-    fun startRecording() {
+    fun play(movement: String) {
         movements.clear()
-        recordingIndex = 0
-        playing = false
         playingIndex = 0
-        recording = true
-    }
-
-    fun stopRecording(name: String) {
-        recording = false
-        recordingIndex = 0
-        playing = false
-        playingIndex = 0
-        val pw = PrintWriter(FileWriter("armageddon/$name.movement"))
-        movements.forEach {
-            pw.println("$recordingIndex;${it.forward};${it.left};${it.backwards};${it.right};${it.sneak};${it.jump};${it.attack};${it.yaw};${it.pitch}")
-            recordingIndex++
-        }
-    }
-
-    fun readRecording(name: String) {
-        movements.clear()
-        recording = false
-        recordingIndex = 0
-        playing = false
-        playingIndex = 0
-        File("armageddon/$name.movement").readLines().forEach {
+        playing = true
+        movement.lines().forEach {
             val split = it.split(";")
-            val movement = Movement(
+            movements.add(Movement(
                 forward = split[1].toBoolean(),
                 left = split[2].toBoolean(),
                 backwards = split[3].toBoolean(),
@@ -92,17 +45,8 @@ class MovementRecorder {
                 attack = split[7].toBoolean(),
                 yaw = split[8].toFloat(),
                 pitch = split[9].toFloat()
-            )
-            movements.add(movement)
+            ))
         }
-    }
-
-    fun play() {
-        playingIndex = 0
-        recording = false
-        recordingIndex = 0
-        playingIndex = 0
-        playing = true
     }
 
     data class Movement(
